135631ef72774ad08cd945c37aa6a8112f0ed2d0
PushEax <pusheax.vk@gmail.com>
2025-05-17 18:16:09 +0300

    add ts for ImageGallery

---
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -0,0 +1,36 @@
+import s from './ImageGallery.module.css';
+import ImageCard from '../ImageCard/ImageCard';
+import { ImageObject } from '../../services/unsplashApi.types';
+import React from 'react';
+
+type Props = {
+  images: ImageObject[];
+  setModal: (src: string) => void;
+};
+
+const ImageGallery = ({ images, setModal }: Props) => {
+  const handleClick = (
+    e: React.MouseEvent<HTMLUListElement, MouseEvent>
+  ): void => {
+    const target = e.target as HTMLElement;
+    if (target.nodeName === 'IMG') {
+      if (target.dataset.src) {
+        setModal(target.dataset.src);
+      }
+    }
+  };
+
+  return (
+    <div className={s.gallery}>
+      <ul className={s.imagesList} onClick={handleClick}>
+        {images.map((image) => (
+          <li key={image.id}>
+            <ImageCard image={image} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ImageGallery;
